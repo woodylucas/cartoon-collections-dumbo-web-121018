359aabe150304fcf138af67abb253554bb13ed8e
Woody Lucas <wlucas0311@gmail.com>
2018-11-26 19:53:14 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,13 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    index += 1
+    puts "#{index}. #{dwarf}"
+  end 
+    
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer)
+  
 end
 
 def long_planeteer_calls# code an argument here

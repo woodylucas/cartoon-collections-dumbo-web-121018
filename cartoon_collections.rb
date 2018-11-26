87f34a2b84cdf8a8d93743d1a692f4ed8ce23ed8
Woody Lucas <wlucas0311@gmail.com>
2018-11-26 20:04:27 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,8 @@ def summon_captain_planet(planeteers)
   
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  
 end
 
 def find_the_cheese# code an argument here

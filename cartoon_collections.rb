1be2aa79d1999e169076dcf2aa67f58c780439dc
Woody Lucas <wlucas0311@gmail.com>
2018-11-26 20:10:26 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def long_planeteer_calls(calls)
   end 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
